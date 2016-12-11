99575dfe246af27d575dc0983131aa0e42365fb5
OsamuMORIMOTO <2525morimoto@gmail.com>
2016-12-11 23:00:39 +0900

    wip

--- NCV.js
+++ NCV.js
@@ -5,8 +5,8 @@ var net = require('net');
 var util = require('util');
 var fs = require('fs');
 
-var HOST = 'msg103.live.nicovideo.jp';
-var PORT = 2833;
+var HOST = '';
+var PORT = 0;
 var thread = '';
 
 var options = {
@@ -80,63 +80,80 @@ var p1 = new Promise(
 });
 
 var p2 = new Promise(function(resolve,reject){
-p1.then(
-function(){
+  p1.then(function(){
 
-  console.log('step2');
+    console.log('step2');
 
-  var req2 = http.request(options2, function(res2) {
-    console.log('get2');
-    console.log(res2.statusCode);
+    var req2 = http.request(options2, function(res2) {
+      console.log('get2');
+      console.log(res2.statusCode);
 
-    var body = '';
-    res2.on('data', function(data){
-      body += data;
+      var body = '';
+      res2.on('data', function(data){
+        body += data;
+      });
+
+      res2.on('end', function(){
+        console.log(body.slice(body.indexOf('<addr>')+6,body.indexOf('</addr>')));
+        console.log(body.slice(body.indexOf('<port>')+6,body.indexOf('</port>')));
+        console.log(body.slice(body.indexOf('<thread>')+8,body.indexOf('</thread>')));
+        HOST = body.slice(body.indexOf('<addr>')+6,body.indexOf('</addr>'));
+        PORT = body.slice(body.indexOf('<port>')+6,body.indexOf('</port>'));
+        thread = body.slice(body.indexOf('<thread>')+8,body.indexOf('</thread>'));
+        resolve();
+      });
+
+    }).on('error', function(e){
+      console.log('error2');
+      console.log(e);
+      reject();
     });
-
-    res2.on('end', function(){
-      console.log(body.slice(body.indexOf('<addr>')+6,body.indexOf('</addr>')));
-      console.log(body.slice(body.indexOf('<port>')+6,body.indexOf('</port>')));
-      console.log(body.slice(body.indexOf('<thread>')+8,body.indexOf('</thread>')));
-      HOST = body.slice(body.indexOf('<addr>')+6,body.indexOf('</addr>'));
-      PORT = body.slice(body.indexOf('<port>')+6,body.indexOf('</port>'));
-      thread = body.slice(body.indexOf('<thread>')+8,body.indexOf('</thread>'));
-      resolve();
-    });
-
-  }).on('error', function(e){
-    console.log('error2');
-    console.log(e);
-    reject();
+    req2.end();
+  }).catch(function(){
+    console.log('catch error');
   });
-  req2.end();
-}).catch(function(){
-  console.log('catch error');
-});
-
 });
 
 Promise.all([p1,p2]).then(function(value){
 
-console.log('step3');
+  console.log('step3');
 
-var client = new net.Socket();
-client.connect(PORT, HOST, function() {
+  var client = new net.Socket();
+  client.connect(PORT, HOST, function() {
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     var sendTag = util.format('<thread res_from="-1000" version="20061206" scores="1" thread="%s" />\0',thread);
     console.log('sendTag:' + sendTag);
     client.write(sendTag);
-});
-client.on('data', function(data) {
-     console.log(data);
-     // Close the client socket completely
-     //client.destroy();
-});
-client.on('close', function() {
+  });
+
+  var dataStock = '';
+  var tmpChat = '';
+  var chatArr = [];
+
+  client.on('data', function(data) {
+    //console.log('' + data);
+    dataStock += data;
+    while(dataStock.search(/\/>/) > 0 || dataStock.search(/<\/chat>/) > 0){
+      dataStock = dataStock.slice(dataStock.search(/\/>/)+2);
+      //console.log(dataStock.slice(0, dataStock.search(/<\/chat>/)+7));
+      tmpChat = dataStock.slice(0, dataStock.search(/<\/chat>/)+7);
+      //console.log(tmpChat);
+      chatArr.push({
+        "no":tmpChat.match(/no=\"(.+?)\"/)[1],
+        "chat":tmpChat.match(/<chat.+?>(.+?)<\/chat>/)[1],
+        "user_id":tmpChat.match(/user_id=\"(.+?)\"/)[1],
+      });
+      console.log(chatArr[chatArr.length-1]["chat"]);
+      dataStock = dataStock.slice(dataStock.search(/<\/chat>/)+7);
+    }
+    // Close the client socket completely
+    //client.destroy();
+  });
+
+  client.on('close', function() {
     console.log('Connection closed');
-});
+  });
 }, function(value){
   console.log('all error');
 });
-
 
